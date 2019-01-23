c0522a979c4c4fda865194eb719a2339e24afe01
Yuan Cao <yuancao85@gmail.com>
2019-01-23 18:17:53 -0500

    Fix comment

--- accumulate.py
+++ accumulate.py
@@ -27,6 +27,6 @@ if __name__ == "__main__":
 		return accumulate(cat, init=new_list, seq=original_list)
 
 	l1 = list(range(5)) # [0, 1, 2, 3, 4]
-	l2 = list(range(5, 10, 1)) # [6, 7, 8, 9]
+	l2 = list(range(5, 10, 1)) # [5, 6, 7, 8, 9]
 
 	assert(extend(l1, l2) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
